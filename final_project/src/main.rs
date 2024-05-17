a6191e108249762d5e9df381c6d0a5361fba36d6
Devon Bedenbaugh <beden2@pdx.edu>
2024-05-16 17:00:26 -0700

    added bad-words api support to final_project

--- final_project/src/main.rs
+++ final_project/src/main.rs
@@ -28,163 +28,58 @@ use core::mem::size_of;
 
 use crate::questions::{Question, QuestionId, Store, Answer, AnswerId};
 use sqlx::{FromRow,postgres::PgPoolOptions};
+use reqwest::Client;
+
+#[derive(Deserialize, Debug)]
+struct FilterResponse{
+    censored_content: String,
+}
+
+#[derive(Deserialize)]
+struct TextContent {
+    text: String,
+}
 
 
+async fn filter_bad_words(text: &str) -> Result<String, Box<dyn std::error::Error>> {
+    dotenv::dotenv().ok();
+    let api_key = std::env::var("BAD_WORDS_API_KEY").expect("BAD_WORDS_API_KEY not set properly.");
+    let client = Client::new();
+    let response = client
+        .post("https://api.apilayer.com/bad_words?censor_character=*")
+        .header("apikey", api_key)
+        .body(serde_json::json!({ "text": text }).to_string())
+        //.json(&serde_json::json!({ "text": text }))
+        .send()
+        .await?;
+    
+    let filter_response: FilterResponse = response.json().await?;
+    println!("bout to return {:?}", filter_response.censored_content);
+    let inner_content: TextContent = serde_json::from_str(&filter_response.censored_content)?;
+    Ok(inner_content.text)
+}
 
 #[derive(Debug)]
 struct InvalidId;
 
 #[derive(Deserialize)]
-struct FormData {
+pub struct FormData {
     title: String,
     content: String,
     tags: String,
 }
 
 #[derive(Deserialize)]
-struct AnswerFormData{
+pub struct AnswerFormData{
     content: String,
     question_id: i32,
 }
 
-#[derive(Deserialize, Serialize, FromRow)]
-struct QuestionAnswer {
-    question_id: Option<i32>,
-    question_content: Option<String>,
-    title: Option<String>,
-    answer_content: Option<String>,
-}
-
-async fn create_db_pool() -> Result<sqlx::Pool<sqlx::Postgres>, sqlx::Error> {
-    let database_url = "postgres://postgres:Password@localhost/final";
-    PgPoolOptions::new()
-        .max_connections(3)
-        .connect(database_url)
-        .await
-}
-
-
-async fn add_question(pool: &sqlx::Pool<sqlx::Postgres>, question: Question) -> Result<(), sqlx::Error>{
-    let tags = question.tags.unwrap_or_else(Vec::new);
-    sqlx::query!(
-        "INSERT INTO questions (title, content, tags) 
-        VALUES ($1, $2, $3)",
-        question.title,
-        question.content,
-        &tags
-    )
-    .execute(pool)
-    .await?;
-    Ok(())
-}
-
-async fn get_questions(
-    Extension(pool): Extension<sqlx::Pool<sqlx::Postgres>>,
-) -> Result <Vec<QuestionAnswer>, sqlx::Error> {
-    let result = sqlx::query_as!(
-        QuestionAnswer,
-        "
-        SELECT 
-            questions.id AS question_id, 
-            questions.content AS question_content,
-            questions.title, 
-            answers.content AS answer_content
-        FROM 
-            questions 
-        FULL JOIN 
-            answers 
-        ON 
-            questions.id = answers.question_id
-        "
-        //Question,
-        //"SELECT * FROM questions"
-    )
-    .fetch_all(&pool)
-    .await?;
-
-    Ok(result)
-}
-
-async fn create_table_questions(
-    Extension(pool): Extension<sqlx::Pool<sqlx::Postgres>>,
-) -> Result<(), sqlx::Error> {
-    sqlx::query!(
-        "CREATE TABLE IF NOT EXISTS questions(
-            id SERIAL PRIMARY KEY,
-            title VARCHAR(255) NOT NULL,
-            content TEXT NOT NULL,
-            tags TEXT[]
-        )"
-    )
-    .execute(&pool)
-    .await?;
-    Ok(())
-}
-
-async fn create_table_answers(
-    Extension(pool): Extension<sqlx::Pool<sqlx::Postgres>>,
-) -> Result<(), sqlx::Error> {
-    sqlx::query!(
-        "CREATE TABLE IF NOT EXISTS answers (
-            id SERIAL PRIMARY KEY,
-            content TEXT NOT NULL,
-            question_id INTEGER,
-            FOREIGN KEY (question_id) REFERENCES questions (id) ON DELETE CASCADE
-        )"
-    )
-    .execute(&pool)
-    .await?;
-    Ok(())
-}
-
-async fn add_answer(
-    pool: &sqlx::Pool<sqlx::Postgres>, 
-    answer: Answer
-) -> Result<(), sqlx::Error> {
-    sqlx::query!(
-        "INSERT INTO answers(id, content, question_id)
-        VALUES ($1, $2, $3)",
-        answer.id, answer.content, answer.question_id
-    )
-    .execute(pool)
-    .await?;
-    Ok(())
-}
-
-async fn delete_question(
-    pool: &sqlx::Pool<sqlx::Postgres>,
-    question: Question,
-) -> Result<(), sqlx::Error> {
-    sqlx::query!(
-        "DELETE FROM questions WHERE id = $1",
-        question.id
-    )
-    .execute(pool)
-    .await?;
-    Ok(())
-}
-
-
-async fn update_table(
-    pool: &sqlx::Pool<sqlx::Postgres>,
-    question: Question,
-) -> Result<(), sqlx::Error> {
-    sqlx::query!(
-        "UPDATE questions
-        SET title = $1, content = $2
-        WHERE id = $3",
-        question.title, question.content, question.id
-    )
-    .execute(pool)
-    .await?;
-    Ok(())
-}
-
 
 async fn get_handler(
-    pool: Extension<sqlx::Pool<sqlx::Postgres>>,
+    Extension(store): Extension<Store>,
 ) -> impl IntoResponse {
-    match get_questions(pool).await {
+    match store.get_questions().await {
         Ok(questions) => {
             let json = serde_json::to_string(&questions).unwrap_or_else(|_| "[]".to_string());
 
@@ -214,16 +109,23 @@ async fn form_handler() -> impl IntoResponse {
 
 async fn post_handler(
     Form(form_data): Form<FormData>,
-    Extension(pool): Extension<sqlx::Pool<sqlx::Postgres>>,
+    Extension(store): Extension<Store>,
 ) -> impl IntoResponse {
+    
+    let filtered_content = match filter_bad_words(&form_data.content).await {
+        Ok(clean_content) => clean_content,
+        Err(_) => return Html("<h1>Error filtering content</h1>"),
+    };
+    
+    
     let tags_vec: Vec<String> = form_data.tags.split(',').map(|s| s.trim().to_string()).collect();
     let new_question = Question {
         id: 1,
         title: form_data.title,
-        content: form_data.content,
+        content: filtered_content,
         tags: Some(tags_vec),
     };
-    match add_question(&pool, new_question).await {
+    match store.add_question(new_question).await {
         Ok(_) => Html("<h1>Question added successfully!</h1>"),
         Err(_) => Html("<h1>Error adding question</h1>"),
     }
@@ -232,7 +134,7 @@ async fn post_handler(
 
 async fn update_handler(
     Form(form_data): Form<FormData>,
-    Extension(pool): Extension<sqlx::Pool<sqlx::Postgres>>,
+    Extension(store): Extension<Store>,
 ) -> impl IntoResponse{
     let tags_vec: Vec<String> = form_data.tags.split(',').map(|s| s.trim().to_string()).collect();
     let new_question = Question {
@@ -241,7 +143,7 @@ async fn update_handler(
         content: form_data.content,
         tags: Some(tags_vec),
     };
-    match update_table(&pool, new_question).await {
+    match store.update_table(new_question).await {
         Ok(_) => Html("<h1>Question added successfully!</h1>"),
         Err(_) => Html("<h1>Error adding question</h1>"),
     }
@@ -250,7 +152,7 @@ async fn update_handler(
 
 async fn delete_handler(
     Form(form_data): Form<FormData>,
-    Extension(pool): Extension<sqlx::Pool<sqlx::Postgres>>,
+    Extension(store): Extension<Store>,
 ) -> impl IntoResponse{
     let tags_vec: Vec<String> = form_data.tags.split(',').map(|s| s.trim().to_string()).collect();
     let new_question = Question {
@@ -259,7 +161,7 @@ async fn delete_handler(
         content: form_data.content,
         tags: Some(tags_vec),
     };
-    match delete_question(&pool, new_question).await {
+    match store.delete_question(new_question).await {
         Ok(_) => Html("<h1>Question added successfully!</h1>"),
         Err(_) => Html("<h1>Error adding question</h1>"),
     }
@@ -273,14 +175,14 @@ async fn answer_form_handler() -> impl IntoResponse {
 
 async fn answer_handler(
     Form(form_data): Form<AnswerFormData>,
-    Extension(pool): Extension<sqlx::Pool<sqlx::Postgres>>,
+    Extension(store): Extension<Store>,
 ) -> impl IntoResponse {
     let new_answer = Answer {
         id: 1,
         content: form_data.content,
         question_id: form_data.question_id,
     };
-    match add_answer(&pool, new_answer).await {
+    match store.add_answer(new_answer).await {
         Ok(_) => Html("<h1>Answer added successfully!</h1>"),
         Err(_) => Html("<h1>Error adding answer</h1>"),
     }
@@ -295,24 +197,21 @@ async fn main() {
     dotenv::dotenv().ok();
     println!("Using database URL: {}", std::env::var("DATABASE_URL").unwrap());
 
-    let pool = match create_db_pool().await {
-        Ok(pool) => pool,
-        Err(e) => {
-            eprintln!("Failed to create database pool: {}", e);
-            return;
-        }
-    };
-    if let Err(e) = create_table_questions(Extension(pool.clone())).await {
+
+
+    let store = Store::new().await.expect("Failed to create store");
+
+    if let Err(e) = store.create_table_questions().await {
         eprintln!("Failed to create questions table: {}", e);
     }
-    if let Err(e) = create_table_answers(Extension(pool.clone())).await {
+    if let Err(e) = store.create_table_answers().await {
         eprintln!("Failed to create answers table: {}", e);
     }
    
 
     let app = Router::new()
 
-    .route("/form", get(form_handler).post(form_handler).put(form_handler).delete(form_handler))
+    .route("/", get(form_handler).post(form_handler).put(form_handler).delete(form_handler))
     .route("/questions/submit", get(post_handler).post(post_handler))
     .route("/questions", get(get_handler))
     .route("/questions/update", get(update_handler).put(update_handler))
@@ -325,164 +224,16 @@ async fn main() {
         ServiceBuilder::new()
             .layer(CorsLayer::new().allow_origin(Any).allow_methods(Any).allow_headers(Any))
             //.layer(Extension(store_filter.clone()))
-            .layer(Extension(pool.clone()))
+            .layer(Extension(store))
     );
 
 
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Server listening on http://{}/form", addr);
+    println!("Server listening on http://{}/", addr);
 
     axum::Server::bind(&addr)
     .serve(app.into_make_service())
     .await
     .expect("Failed to start server");
 }
-
-/* 
-
-///GET Handler
-
-
-async fn get_hr(
-    Extension(store): Extension<Arc<Mutex<Store>>>,
-) -> impl IntoResponse{
-    let mut store = store.lock().await;
-
-    println!("get_hr called");
-
-    match store.get_questions_json().await {
-
-        Ok(questions) => {
-            Response::builder()
-                .status(StatusCode::OK)
-                .header("content-type", "application/json")
-                .body(questions)
-                .unwrap()
-        }
-        Err(err) => {
-            eprintln!("Error parsing JSON: {}", err);
-
-            let error_response = "Error fetching and parsing questions".to_string();
-            Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .body(error_response)
-                .unwrap()
-        }
-    }
-}
-
-
-
-///POST Handler
-
- 
-async fn post_hr(
-    Extension(store): Extension<Arc<Mutex<Store>>>,
-    //Json(question): Json<Question>
-)   -> impl IntoResponse {
-    println!("post_hr called");
-    let mut store = store.lock().await;
-    let new_question = Question::new(
-        QuestionId::from_str("3").expect("No id provided"),
-        "3nd Question".to_string(),
-        "content, question3".to_string(),
-            Some(vec!("faq".to_string())),  //encapsulate and create a vector
-        );
-    match store.add_question(new_question).await{
-    //(StatusCode::CREATED, "Question added successfully.")
-        Ok(_) => {
-        Response::builder()
-                    .status(StatusCode::OK)
-                    .header("content-type", "application/json")
-                    .body("Created successfully".to_string())
-                    .unwrap()   
-            }
-        Err(err) => {
-            //eprintln!("Error posting JSON: ");
-
-            let error_response = "Error creating and submitting question".to_string();
-            Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .body(error_response)
-                .unwrap()
-            }
-        }
-}
-
-
-
-
-///PUT Handler
-async fn update_hr(
-    Extension(store): Extension<Arc<Mutex<Store>>>,
-    //Json(question): Json<Question>
-    Path(question_id): Path<QuestionId>,
-) -> impl IntoResponse {
-    println!("update_hr called");
-
-    let mut store = store.lock().await;
-    let question = Question::new(
-        QuestionId::from_str("1").expect("No id provided"),
-        "First Question UPDATED".to_string(),
-        "Content of question".to_string(),
-            Some(vec!("faq".to_string())),  //encapsulate and create a vector
-        );
-
-
-    match store.update_question(&question_id, question).await {
-        Ok(_) => Response::builder()
-                        .status(StatusCode::OK)
-                        .header("content-type", "application/json")
-                        .body("Question updated successfully.".to_string())
-                        .unwrap(),
-        Err(error) => Response::builder()
-                        .status(StatusCode::NOT_FOUND)
-                        .header("content-type", "application/json")
-                        .body(error.to_string())
-                        .unwrap(),
-    }
-
-
-}
-
-///DELETE Handler
-
-async fn delete_hr(
-    Extension(store): Extension<Arc<Mutex<Store>>>,
-    Path(question_id): Path<QuestionId>
-) -> impl IntoResponse {
-    let mut store = store.lock().await;
-    match store.delete_question(&question_id).await {
-        Ok(_) => (StatusCode::OK, "Question deleted successfully.".to_string()),
-        Err(error) => (StatusCode::NOT_FOUND, error),
-    }
-}
-
-async fn answer_hr(
-    Extension(store): Extension<Arc<Mutex<Store>>>,
-    //Path(answer_id): Path<AnswerId>,
-    
-
-) -> impl IntoResponse{
-    let mut store = store.lock().await;
-
-    let answer= Answer::new(
-        AnswerId("1".to_string()),
-        "test".to_string(),
-        //params.get("content").unwrap().to_string(),
-        QuestionId("101".to_string()),
-    );
-
-
-    match store.add_answer(answer).await{
-        Ok(response) => {
-            (StatusCode::OK, "answer added successfully.".to_string())
-        }
-        Err(e) => {
-            (StatusCode::NOT_FOUND, e)        }
-    }
-
-}
-
-*/
--- final_project/src/questions.rs
+++ final_project/src/questions.rs
@@ -12,7 +12,7 @@ use serde::{Serialize, Deserialize};
 //use tower::ServiceBuilder;
 use std::collections::HashMap;
 //use core::mem::size_of;
-use sqlx::postgres::PgPoolOptions;
+use sqlx::{FromRow,postgres::PgPoolOptions};
 
 
 
@@ -70,18 +70,164 @@ impl Question {
     }
 }
 
+#[derive(Deserialize, Serialize, FromRow)]
+pub struct QuestionAnswer {
+    question_id: Option<i32>,
+    question_content: Option<String>,
+    title: Option<String>,
+    answer_content: Option<String>,
+}
+
 
 ///Structure for loading and saving database.
 /// #handles basic CRUD components
 /// 
-//#[derive(Clone)]
+#[derive(Clone)]
 pub struct Store{
-    questions: Mutex<HashMap<QuestionId, Question>>,
-    answers: Mutex<HashMap<AnswerId, Answer>>,
+    pub pool: sqlx::Pool<sqlx::Postgres>,
 }
 
+impl Store{
+
+    pub async fn new() -> Result<Self, sqlx::Error> {
+        let database_url = "postgres://postgres:Password@localhost/final";
+        let pool = PgPoolOptions::new()
+            .max_connections(3)
+            .connect(database_url)
+            .await?;
+        Ok(Store {pool})
+    }
+
+    pub async fn get_pool(&self) -> sqlx::Pool<sqlx::Postgres> {
+        self.pool.clone()
+    }
+
+    pub async fn add_question(&self, question: Question) -> Result<(), sqlx::Error>{
+        let tags = question.tags.unwrap_or_else(Vec::new);
+        sqlx::query!(
+            "INSERT INTO questions (title, content, tags) 
+            VALUES ($1, $2, $3)",
+            question.title,
+            question.content,
+            &tags
+        )
+        .execute(&self.pool)
+        .await?;
+        Ok(())
+    }
+
+    pub async fn update_table(
+        &self,
+        question: Question,
+    ) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            "UPDATE questions
+            SET title = $1, content = $2
+            WHERE id = $3",
+            question.title, question.content, question.id
+        )
+        .execute(&self.pool)
+        .await?;
+        Ok(())
+    }
+
+        
+    pub async fn delete_question(
+        &self,
+        question: Question,
+    ) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            "DELETE FROM questions WHERE id = $1",
+            question.id
+        )
+        .execute(&self.pool)
+        .await?;
+        Ok(())
+    }
 
 
+    pub async fn add_answer(
+        &self, 
+        answer: Answer
+    ) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            "INSERT INTO answers(id, content, question_id)
+            VALUES ($1, $2, $3)",
+            answer.id, answer.content, answer.question_id
+        )
+        .execute(&self.pool)
+        .await?;
+        Ok(())
+    }
+    
+
+
+    pub async fn get_questions(
+        &self,
+    ) -> Result <Vec<QuestionAnswer>, sqlx::Error> {
+        let result = sqlx::query_as!(
+            QuestionAnswer,
+            "
+            SELECT 
+                questions.id AS question_id, 
+                questions.content AS question_content,
+                questions.title, 
+                answers.content AS answer_content
+            FROM 
+                questions 
+            FULL JOIN 
+                answers 
+            ON 
+                questions.id = answers.question_id
+            "
+            //Question,
+            //"SELECT * FROM questions"
+        )
+        .fetch_all(&self.pool)
+        .await?;
+    
+        Ok(result)
+    }
+
+
+
+
+
+
+
+
+    pub async fn create_table_questions(
+        &self,
+    ) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            "CREATE TABLE IF NOT EXISTS questions(
+                id SERIAL PRIMARY KEY,
+                title VARCHAR(255) NOT NULL,
+                content TEXT NOT NULL,
+                tags TEXT[]
+            )"
+        )
+        .execute(&self.pool)
+        .await?;
+        Ok(())
+    }
+    
+    pub async fn create_table_answers(
+        &self,
+    ) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            "CREATE TABLE IF NOT EXISTS answers (
+                id SERIAL PRIMARY KEY,
+                content TEXT NOT NULL,
+                question_id INTEGER,
+                FOREIGN KEY (question_id) REFERENCES questions (id) ON DELETE CASCADE
+            )"
+        )
+        .execute(&self.pool)
+        .await?;
+        Ok(())
+    }
+}
     
 /* 
 impl FromStr for QuestionId {
